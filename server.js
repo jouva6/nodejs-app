6fc250005f84bfa1357134c650aab4370a5187d7
root <root@localhost.localdomain>
2025-09-18 10:00:28 +0200

    Fix syntax error in server.js

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.get('/', (req, res) => {
-  res.send('
-    <h1 style="color:red;">Bienvenue sur mon app Node.js modifiée</h1>
-    <p style="color:purple;">Page mise à jour</p>
-    ');
+res.send('<h1 style="color:red;">Bienvenue sur mon app Node.js modifiée</h1><p style="color:purple;">Page mise à jour</p');
 
 });
 app.listen(port, () => console.log(`App running on port ${port}`));
