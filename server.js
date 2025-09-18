7c978f14eece70f4b7dad5a5dc91ec68c9e6bd81
root <root@localhost.localdomain>
2025-09-18 09:43:16 +0200

    mise a jour du conde node.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.get('/', (req, res) => {
-res.send('<h1>Bienvenue sur mon app Node.js simple avec OpenShift</
-h1>');
+res.send('<h1 style="color:red;">Bienvenue sur mon app Node.js modifiée</
+h1><p style="color:purple;">Page mise à jour</p>');
+
 });
 app.listen(port, () => console.log(`App running on port ${port}`));
